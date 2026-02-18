f1582dd61361cc608f5bfb850bc92209a137dab5
Connor Shepherd <cjshep@iastate.edu>
2026-02-18 12:40:43 -0600

    First 2 examples

---
+++ Simple MNIST Convnet/MNIST.py
@@ -0,0 +1,104 @@
+import numpy as np
+import keras
+from keras import layers
+import matplotlib.pyplot as plt
+
+# Data prep
+# Model / data parameters
+num_classes = 10
+input_shape = (28, 28, 1)
+
+# Load the data and split between train and test sets
+(x_train, y_train), (x_test, y_test) = keras.datasets.mnist.load_data()
+
+# Scale images to the [0, 1] range
+x_train = x_train.astype("float32") / 255
+x_test = x_test.astype("float32") / 255
+# Make sure images have shape (28, 28, 1)
+x_train = np.expand_dims(x_train, -1)
+x_test = np.expand_dims(x_test, -1)
+print("x_train shape:", x_train.shape)
+print(x_train.shape[0], "train samples")
+print(x_test.shape[0], "test samples")
+
+# Convert class vectors to binary class matrices
+y_train = keras.utils.to_categorical(y_train, num_classes)
+y_test = keras.utils.to_categorical(y_test, num_classes)
+
+# Build the model
+model = keras.Sequential(
+    [
+        keras.Input(shape=input_shape),
+        layers.Conv2D(32, kernel_size=(3, 3), activation="relu"),
+        layers.MaxPooling2D(pool_size=(2, 2)),
+        layers.Conv2D(64, kernel_size=(3, 3), activation="relu"),
+        layers.MaxPooling2D(pool_size=(2, 2)),
+        layers.Flatten(),
+        layers.Dropout(0.5),
+        layers.Dense(num_classes, activation="softmax"),
+    ]
+)
+model.summary()
+
+# Train the model
+batch_size = 128
+epochs = 15
+
+model.compile(loss="categorical_crossentropy", optimizer="adam", metrics=["accuracy"])
+history = model.fit(x_train, y_train, batch_size=batch_size, epochs=epochs, validation_split=0.1)
+
+# Evaluate the model
+score = model.evaluate(x_test, y_test, verbose=0)
+print("Test loss:", score[0])
+print("Test accuracy:", score[1])
+
+# Plot the model architecture
+keras.utils.plot_model(model, to_file='model_architecture.png', show_shapes=True)
+
+# Plot training history
+plt.figure(figsize=(12, 4))
+plt.subplot(1, 2, 1)
+plt.plot(history.history['loss'], label='Train Loss')
+plt.plot(history.history['val_loss'], label='Val Loss')
+plt.title('Loss')
+plt.xlabel('Epoch')
+plt.ylabel('Loss')
+plt.legend()
+
+plt.subplot(1, 2, 2)
+plt.plot(history.history['accuracy'], label='Train Accuracy')
+plt.plot(history.history['val_accuracy'], label='Val Accuracy')
+plt.title('Accuracy')
+plt.xlabel('Epoch')
+plt.ylabel('Accuracy')
+plt.legend()
+plt.tight_layout()
+plt.show()
+
+# Predictions on test data
+predictions = model.predict(x_test[:10])
+for i in range(10):
+    plt.subplot(2, 5, i+1)
+    plt.imshow(x_test[i].squeeze(), cmap='gray')
+    plt.title(f"Pred: {np.argmax(predictions[i])}")
+    plt.axis('off')
+plt.tight_layout()
+plt.show()
+
+# Activation maps
+layer_outputs = [layer.output for layer in model.layers if 'conv' in layer.name]
+activation_model = keras.Model(inputs=model.input, outputs=layer_outputs)
+activations = activation_model.predict(x_test[0:1])
+
+plt.figure(figsize=(12, 4))
+plt.subplot(1, 2, 1)
+plt.imshow(activations[0].squeeze(), cmap='gray')
+plt.title("Activation map")
+plt.axis('off')
+
+plt.subplot(1, 2, 2)
+plt.imshow(activations[1].squeeze(), cmap='gray')
+plt.title("Activation map")
+plt.axis('off')
+plt.tight_layout()
+plt.show()
